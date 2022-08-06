f518f415774b85443eeccf5c2e6197c8be6d25ae
Arkashka <sevenkapsul@mail.ru>
2022-08-06 15:23:48 +0300

    Task_36_Completed

--- Job/36-Zadahca/Program.cs
+++ Job/36-Zadahca/Program.cs
@@ -8,3 +8,35 @@
 [3, 7, 23, 12] -> 19
 [-4, -6, 89, 6] -> 0
 */
+
+Random rnd = new Random();
+
+int[] Massive(int len)
+{
+    int Summa = 0;
+    int[] massive = new int[len];
+
+    Console.WriteLine("Массив:");
+    for (int i = 0; i < len; i++)
+    {
+        massive[i] = rnd.Next(-10, 10);
+        Console.Write(massive[i] + " ");
+    }
+    Console.WriteLine();
+   
+    Console.WriteLine("Числа с не чётными индексами массива:");
+    for (int i = 0; i < len; i++)
+    {
+        if (i % 2 != 0)
+        {
+            Summa += massive[i];
+            Console.Write(massive[i] + " ");
+        }
+    }
+
+    Console.WriteLine();
+    Console.WriteLine($"Сумма чисел, с не четными индексами равна: {Summa}");
+    return massive;
+}
+// Вызов метода:
+Massive(10);
