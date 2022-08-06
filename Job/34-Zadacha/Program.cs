2fc3106185e43a626b4e1763a68c454d0b2a75b8
Arkashka <sevenkapsul@mail.ru>
2022-08-06 14:59:03 +0300

    Task_34_Completed

--- Job/34-Zadacha/Program.cs
+++ Job/34-Zadacha/Program.cs
@@ -8,3 +8,35 @@
 Пример:
 [345, 897, 568, 234] -> 2
 */
+
+Random rnd = new Random();
+
+int[] Massive(int len)
+{
+    int CountChetNumber = 0;
+    int[] massive = new int[len];
+
+    Console.WriteLine("Массив:");
+    for (int i = 0; i < len; i++)
+    {
+        massive[i] = rnd.Next(100, 1000);
+        Console.Write(massive[i] + " ");
+    }
+    Console.WriteLine();
+   
+    Console.WriteLine("Чётные числа Массива:");
+    for (int i = 0; i < len; i++)
+    {
+        if (massive[i] % 2 == 0)
+        {
+            CountChetNumber++;
+            Console.Write(massive[i] + " ");
+        }
+    }
+
+    Console.WriteLine();
+    Console.WriteLine($"Число чётных чисел в данном массиве равна: {CountChetNumber}");
+    return massive;
+}
+// Вызов метода:
+Massive(100);
