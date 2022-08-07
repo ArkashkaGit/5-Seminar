716e299b397a403f77744133f23762c27aa940da
Arkashka <sevenkapsul@mail.ru>
2022-08-07 16:37:43 +0300

    Task_Dop-Zadacha_Completed

--- Job/Dop-Zadacha/Program.cs
+++ Job/Dop-Zadacha/Program.cs
@@ -9,12 +9,51 @@
 [1,2,3,4,5,6,7,8]
 */
 
-/*
-int[] array1 = {1,3,5,7};
-int[] array2 = {2,4,6,8};
+int[] massive1 = { 1, 3, 5, 7 };
+int[] massive2 = { 2, 4, 6, 8 };
+int a = massive1.Length;
+int b = massive2.Length;
+int c = a + b;
+int[] sortMassive = new int[c];
 
-for (int i = 0; i < array1.Length; i++) {
-    Console.Write(array1[i] + " ");
-    Console.Write(array2[i] + " ");
+//Объединение массивов...
+
+Console.WriteLine("Объединение массивов:");
+for (int i = 0; i < massive1.Length; i++)
+{
+    sortMassive[i] = massive1[i];
 }
-*/
+
+for (int i = 0; i < massive2.Length; i++)
+{
+    sortMassive[a + i] = massive2[i];
+}
+
+for (int i = 0; i < sortMassive.Length; i++)
+{
+    Console.Write(sortMassive[i] + " ");
+}
+
+// Сортировка методом:
+//Array.Sort(newMassive);
+
+// Сортировка кодом:
+
+for (int i = 0; i < sortMassive.Length; i++)
+{
+    for (int j = 0; j < sortMassive.Length - 1; j++)
+    {
+        if (sortMassive[j] > sortMassive[j + 1])
+        {
+            int num = sortMassive[j];
+            sortMassive[j] = sortMassive[j + 1];
+            sortMassive[j + 1] = num;
+        }
+    }
+}
+Console.WriteLine();
+Console.WriteLine("Отсортированный массив:");
+for (int i = 0; i < sortMassive.Length; i++)
+{
+    Console.Write(sortMassive[i] + " ");
+}
